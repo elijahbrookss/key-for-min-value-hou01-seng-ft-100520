c7f655035f799a39b4b7ae74940e3d5db19a4eb7
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-19 14:28:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.each do |key, value|
+    least_value = value;
+      p least_value
+  end
 end
+
+key_for_min_value({"apple" => -45, "banana" => -44.5, "carrot" => -44.9})
