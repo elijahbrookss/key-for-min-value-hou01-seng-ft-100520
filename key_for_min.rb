da0b0b213fb7bcd2fd3091c236aa0af0b5b9b347
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-19 14:29:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,16 +8,18 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if count == 0 then
       least_value = value;
+      least_key = key;
+
       count += 1;
     else
       if value < least_value then
         least_value = value;
-        least_key = key
+        least_key = key;
       end
     end
   end
   
-  return least_value;
+  return least_key;
 end
 
 p key_for_min_value({"apple" => -45, "banana" => -44.5, "carrot" => -44.9})
